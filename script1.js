c2e3ecd37bc49e99839f668e8004f207ba9365ed
TuralJafar <76geemk@code.edu.az>
2023-02-25 22:23:59 +0300

    initial commit

---
+++ script1.js
@@ -0,0 +1,12 @@
+let n=1991;
+let a = n/1000; //a=1
+let b = n/100%10; //b=9
+let c =n/10%10; //c= 9
+let d = n%10; //d=1
+if (a==d && b==c)
+{
+    console.log("yes");
+}
+    else {
+    console.log("no");
+}    
